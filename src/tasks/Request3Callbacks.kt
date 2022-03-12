88f7134fd53d2d944964807784cdfca9957b7b96
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-12 11:44:56 +0100

    first attempt- callbacks

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -4,15 +4,13 @@ import contributors.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.util.*
-import java.util.concurrent.atomic.AtomicInteger
 
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     service.getOrgReposCall(req.org).onResponse { responseRepos ->
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
         val allUsers = mutableListOf<User>()
-        for (repo in repos) {
+/*        for (repo in repos) {
             service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
@@ -21,6 +19,18 @@ fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateRe
         }
         // TODO: Why this code doesn't work? How to fix that?
         updateResults(allUsers.aggregate())
+        */
+        for ((index, repo) in repos.withIndex()) {   // #1
+            service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
+                logUsers(repo, responseUsers)
+                val users = responseUsers.bodyList()
+                allUsers += users
+                if (index == repos.lastIndex) {      // #2
+                    updateResults(allUsers.aggregate())
+                }
+            }
+        }
+
     }
 }
 
