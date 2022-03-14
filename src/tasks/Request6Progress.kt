b4ce1b4f1c22e7a686f2e8733a3dea39b7df391d
mauriziosodano <maurizio.sodano@gmail.com>
2022-03-14 08:53:56 +0100

    updating UI with progress

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -2,10 +2,35 @@ package tasks
 
 import contributors.*
 
+val utmRepos = listOf(
+    "asprid-flutter",
+    "utm-simulator-planner-node",
+    "asprid_configurator",
+    "asprid-server",
+    "eARTS",
+    "automated-workflow",
+    "asprid-file-storage",
+    "utm-engine-server",
+    "utm-realtime-ui"
+)
+
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .bodyList().filter { repo-> utmRepos.contains(repo.name) }
+
+    var allUsers = emptyList<User>()
+    for ((index, repo) in repos.withIndex()) {
+        val users = service.getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+
+        allUsers = (allUsers + users).aggregate()
+        updateResults(allUsers, index == repos.lastIndex)
+    }
 }
